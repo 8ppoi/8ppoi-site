543f332baee7a0e6357519820a3d57789a2dfa93
Kenta Saito <kenta.saito@sagittar.org>
2023-09-11 16:44:17 +0900

    2023-09-11 16:04:17

---
+++ index.js
@@ -0,0 +1,11 @@
+import { Application } from 'https://deno.land/x/oak/mod.ts';
+
+const app = new Application();
+app.use(async context => {
+	await context.send({
+		root: `${Deno.cwd()}`,
+		index: 'index.html',
+	});
+});
+
+await app.listen({ port: 8000 });
